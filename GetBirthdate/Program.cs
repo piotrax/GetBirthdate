6511c94eda86b78268b728388595e80f6e871a12
Piotr <piotrax@gmail.com>
2023-11-07 19:18:23 +0100

    v.1.2 changed date parsing

--- GetBirthdate/Program.cs
+++ GetBirthdate/Program.cs
@@ -12,9 +12,10 @@ namespace GetBirthdate
             Console.WriteLine("'--------------------------------------------------------'\n");
             // program pobiera prawidłowe daty począwszy od 01.01.1900 r.
 
-            while (true)
+            while (true)    
             {
                 Console.Write("Podaj datę urodzin: ");
+                /* wyrażenie regularne sprawdzające datę ze strony: https://regex101.com/r/hZ3nT4/1 */
                 string pattern = @"(^(((0[1-9]|1[0-9]|2[0-8])[\.](0[1-9]|1[012]))|((29|30|31)[\.](0[13578]|1[02]))|((29|30)[\.](0[4,6,9]|11)))[\.](19|[2-9][0-9])\d\d$)|(^29[\.]02[\.](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)";
                 string input = Console.ReadLine();
                 if (input == "q") break;
